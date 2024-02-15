e1150865d20f9d3b34cedc26842015c632561310
MDshirzad <mohamadshirzad81@gmail.com>
2024-02-15 10:35:15 +0330

    Done

--- Bank/Model/Da/DB.cs
+++ Bank/Model/Da/DB.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,14 @@ namespace Bank.Model.Da
 {
     public class DB:DbContext
     {
-        public DB():base("condb")
+        public DB():base(getString())
         {
         }
+      static  private string getString()
+        {
 
+            return $"Data Source=.;Initial Catalog=Bank;AttachDbFilename={Directory.GetCurrentDirectory()}/CardData.mdf;Integrated Security=true";
+        }
         public DbSet<Person>People { get; set; }
 
         public DbSet<Card> Cards { get; set; }
